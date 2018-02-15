65dd7226a56dfbace3445c5c9389ce82b907bdf1
Simon Siefke <simon.siefke@gmail.com>
2018-02-15 19:36:16 +0100

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
+
 module.exports = {
   lintOnSave: true,
   dll: true, // faster compilation
   devServer: {
+    /* https is needed for excel-plugins to work.
+       you need generate a cert-file and a key-file and
+       place it inside the ssl folder */
     https: {
       cert: fs.readFileSync('./ssl/server.crt'),
       key: fs.readFileSync('./ssl/server.key'),
     },
     open: true, // opens application in browser
     port: 3000,
-    // headers: {
-    //   'Access-Control-Allow-Origin': '*',
-    // },
   },
 }
